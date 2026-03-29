6d20dce6bde4791eeb2bffa3f20628d3a925ca1f
Alexander Petranov <shasho68.5@gmail.com>
2026-03-29 13:39:44 +0300

    Create cards array

---
+++ src/Cards.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const cards = [
+    {
+        emoji: '🤔',
+        flipped: false
+    },
+    {
+        emoji: '🎞️',
+        flipped: false
+    },
+    {
+        emoji: '🤔',
+        flipped: false
+    },
+    {
+        emoji: '🎞️',
+        flipped: false
+    },
+    {
+        emoji: '💰',
+        flipped: false
+    },
+    {
+        emoji: '💰',
+        flipped: false
+    },
+]
+
+function Cards() {
+  return (
+    <div>Cards</div>
+  )
+}
+
+export default Cards
