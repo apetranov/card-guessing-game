45fce5bd9009c6b1f565c0249dee2ebef41da684
Alexander Petranov <shasho68.5@gmail.com>
2026-03-29 15:10:46 +0300

    Start working on game logic

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState } from 'react';
 import './App.css'
 
-const cards = [
+function Cards() {
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [flippedCardsIndex, setFlippedCardsIndex] = useState(0);
+    const [flippedCount, setFlippedCount] = useState(0);
+    const [prevFlipped, setPrevflipped] = useState('');
+    const [flipped, setFlipped] = useState('');
+    const [cards, setCards] = useState([
     {
         id: 1,
         emoji: '🤔',
@@ -32,13 +39,52 @@ const cards = [
         emoji: '💰',
         flipped: false
     },
-]
+]);
 
-function Cards() {
+    function handleFlip(toFlipId) {
+        const cardsCopy = [...cards];
+        const flippedCardsCopy = [...flippedCards];
+
+        
+
+        for (let i = 0; i < cardsCopy.length; i++) {
+            if (cardsCopy[i].id === toFlipId) {
+                if (!flippedCardsCopy.length) {
+                    cardsCopy[i].flipped = true;
+                    flippedCardsCopy.push(cardsCopy[i]);
+                    
+                } else if (flippedCardsCopy.length) {
+                    if (cardsCopy[i].emoji === flippedCardsCopy[flippedCardsIndex].emoji) {
+                        cardsCopy[i].flipped = true;
+                        flippedCardsCopy.push(cardsCopy[i]);
+                        setFlippedCardsIndex(flippedCardsIndex + 1);
+                    }
+                }
+            }
+        }
+
+        console.log(flippedCardsCopy);
+
+        // console.log(cardsCopy);
+        setFlippedCards(flippedCardsCopy);
+        setCards(cardsCopy);
+    }
+
   return (
     <div className='cards-grid'>
         {cards.map(card => (
-            <div className='card' key={card.id}>{card.flipped && card.emoji}</div>
+            <div 
+                className='card' 
+                key={card.id}
+                onClick={() => {
+                    // console.log(`card ${card.id} clicked`);
+                    handleFlip(card.id);
+                    setFlipped(card.emoji);
+                }}
+                >
+                
+                {card.flipped && card.emoji}
+            </div>
         ))}
     </div>
   )
