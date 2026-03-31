ab4ff5f8b9fb3083a7dc609e54dc02993a44413a
Alexander Petranov <shasho68.5@gmail.com>
2026-03-31 16:13:28 +0300

    Almost finish logic

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,13 +1,12 @@
 import React, { use } from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function Cards() {
+    const [firstFlippedCard, setFirstFlippedCard] = useState();
+    const [secondFlippedCard, setSecondFlippedCard] = useState();
     const [flippedCards, setFlippedCards] = useState([]);
     const [flippedCardsIndex, setFlippedCardsIndex] = useState(0);
-    const [flippedCount, setFlippedCount] = useState(0);
-    const [prevFlipped, setPrevflipped] = useState('');
-    const [flipped, setFlipped] = useState('');
     const [cards, setCards] = useState([
     {
         id: 1,
@@ -41,49 +40,116 @@ function Cards() {
     },
 ]);
 
-    function handleFlip(toFlipId) {
-        const cardsCopy = [...cards];
-        const flippedCardsCopy = [...flippedCards];
+    // function handleFlip(toFlipId) {
+    //     const cardsCopy = [...cards];
+    //     const flippedCardsCopy = [...flippedCards];
 
+
+    // }
+
+    
+
+    // function handleFlip() {
         
+    //     if (firstFlippedCard && secondFlippedCard && firstFlippedCard.emoji === secondFlippedCard.emoji) {
+    //         console.log("Cards match!", firstFlippedCard, secondFlippedCard);
+    //     }
+    //     else {
+    //         console.log('Two cards are not flipped atm');
+    //     }
+    // }
 
-        for (let i = 0; i < cardsCopy.length; i++) {
-            if (cardsCopy[i].id === toFlipId) {
-                if (!flippedCardsCopy.length) {
-                    cardsCopy[i].flipped = true;
-                    flippedCardsCopy.push(cardsCopy[i]);
-                    
-                } else if (flippedCardsCopy.length) {
-                    if (cardsCopy[i].emoji === flippedCardsCopy[flippedCardsIndex].emoji) {
-                        cardsCopy[i].flipped = true;
-                        flippedCardsCopy.push(cardsCopy[i]);
-                        setFlippedCardsIndex(flippedCardsIndex + 1);
-                    }
-                }
+    function isFlippedCard(id) {
+        for (let i = 0; i < flippedCards.length; i++) {
+            if (flippedCards[i].id === id) {
+                return true;
             }
         }
-
-        console.log(flippedCardsCopy);
-
-        // console.log(cardsCopy);
-        setFlippedCards(flippedCardsCopy);
-        setCards(cardsCopy);
+        return false;
     }
+
+    useEffect(() => {
+        if (firstFlippedCard && secondFlippedCard && firstFlippedCard.emoji === secondFlippedCard.emoji 
+            && firstFlippedCard.id !== secondFlippedCard.id
+        ) {
+            console.log("Cards match!", firstFlippedCard, secondFlippedCard);
+
+            const copyArr = [...flippedCards];
+
+            let firstCardIdFound = false;
+            let secondCardIdFound = false;
+
+            for (let i = 0; i < copyArr.length; i++) {
+                if (copyArr[i].id === firstFlippedCard.id) {
+                    firstCardIdFound = true;
+                }
+            }
+
+            for (let i = 0; i < copyArr.length; i++) {
+                if (copyArr[i].id === secondFlippedCard.id) {
+                    secondCardIdFound = true;
+                }
+            }
+
+            if (!firstCardIdFound) {
+                copyArr.push(firstFlippedCard);
+            }
+
+            if (!secondCardIdFound) {
+                copyArr.push(secondFlippedCard);
+            }
+
+            console.log("Flipped cards arr: ", copyArr);
+
+            setFlippedCards(copyArr);
+            setFirstFlippedCard();
+            setSecondFlippedCard();
+
+        } else if (firstFlippedCard && secondFlippedCard && firstFlippedCard.emoji !== secondFlippedCard.emoji) {
+            console.log("Cards don't match!", firstFlippedCard, secondFlippedCard);
+            setFirstFlippedCard();
+            setSecondFlippedCard();
+            
+        } else if (firstFlippedCard && secondFlippedCard && firstFlippedCard.id === secondFlippedCard.id) {
+            console.log("Cannot select same card twice!");
+            setFirstFlippedCard();
+            setSecondFlippedCard();
+        } else {
+            console.log('Two cards are not flipped atm');
+        }
+    }, [firstFlippedCard, secondFlippedCard])
+
+
 
   return (
     <div className='cards-grid'>
-        {cards.map(card => (
+        {cards.map((card) => (
             <div 
                 className='card' 
                 key={card.id}
                 onClick={() => {
                     // console.log(`card ${card.id} clicked`);
-                    handleFlip(card.id);
-                    setFlipped(card.emoji);
+                    // handleFlip(card.id);
+                    let cardCopy = { ...card };;
+                    if (!firstFlippedCard) {
+                        console.log(card.id);
+                        // card.flipped = true;
+                        cardCopy.flipped = true;
+                        setFirstFlippedCard(cardCopy);
+                    } else if (firstFlippedCard && !secondFlippedCard) {
+                        // card.flipped = true;
+                        cardCopy.flipped = true;
+                        setSecondFlippedCard(cardCopy);
+                    }
+
                 }}
                 >
                 
-                {card.flipped && card.emoji}
+                {
+                    isFlippedCard(card.id) ? card.emoji 
+                    : firstFlippedCard && firstFlippedCard.id === card.id 
+                    ? card.emoji : ''
+                }
             </div>
         ))}
     </div>
